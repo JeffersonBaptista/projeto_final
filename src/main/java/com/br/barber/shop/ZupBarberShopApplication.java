cdb6f38eb411c8bd8c5f1b43247758ea8bd7dd00
jeffersonbaptista <jefferson.baptista@zup.com.br>
2019-07-31 08:24:46 -0300

    iniciar projeto e configurar banco

---
+++ src/main/java/com/br/barber/shop/ZupBarberShopApplication.java
@@ -0,0 +1,13 @@
+package com.br.barber.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupBarberShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupBarberShopApplication.class, args);
+	}
+
+}
