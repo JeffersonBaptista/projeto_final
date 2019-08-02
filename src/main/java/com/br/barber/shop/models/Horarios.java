932c0c4c0af868a807d3703f29bf5e2437998c5b
jeffersonbaptista <jefferson.baptista@zup.com.br>
2019-08-02 14:44:44 -0300

    criar pacotes

---
+++ src/main/java/com/br/barber/shop/models/Horarios.java
@@ -0,0 +1,19 @@
+package com.br.barber.shop.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Horarios implements Serializable{
+	private static final long serilVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+
+}
